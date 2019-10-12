ebdb570c8d068593b97b60579505e31f69004752
charanpreetkaur026 <charanpreetkaur026@gmail.com>
2019-10-11 21:28:58 -0400

    [charan] checking clone

---
+++ PayRoll_Swift/CommissionBased.swift
@@ -0,0 +1,13 @@
+//
+//  CommissionBased.swift
+//  PayRoll_Swift
+//
+//  Created by MacStudent on 2019-10-11.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+class CommissionBased{
+    print("this is charan creating class CommissionBased")
+}
